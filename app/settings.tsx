746de9a712155a9dab7e567d7291a4342b5aa47b
Katanoisi23 <katanoisi23@gmail.com>
2026-04-10 21:01:01 -0700

    1
    11

--- app/settings.tsx
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView, Text } from "react-native";
-
-export default function SettingsScreen() {
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#F5E9DC" }}>
-      <Text style={{ marginTop: 50, marginLeft: 20 }}>Настройки</Text>
-    </SafeAreaView>
-  );
-}
